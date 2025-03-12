53359b07e4b1a8debbeef67ae45e038ea5a3b084
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-12 17:29:45 -0300

    Criando o DTO de atualização de produtos

--- src/api/produtos/dto/update-produto.dto.ts
+++ src/api/produtos/dto/update-produto.dto.ts
@@ -2,7 +2,7 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateProdutoDto } from './create-produto.dto';
 
 export class UpdateProdutoDto extends PartialType(CreateProdutoDto) {
-    nome: string;
-    descricao: string;
-    preco: number;
+    nome: string
+    descricao: string
+    preco: number
 }
