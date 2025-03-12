297aaa48999e3c4ff8eea23d9a39a386c70f05a5
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-12 18:39:50 -0300

    Adicoinando o modulo de cargos no modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutosModule } from './api/produtos/produtos.module';
+import { CargosModule } from './api/cargos/cargos.module';
 
 @Module({
-  imports: [ProdutosModule],
+  imports: [ProdutosModule, CargosModule],
   controllers: [AppController],
   providers: [AppService],
 })
